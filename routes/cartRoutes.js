fa27f5ef061bd1bc004fd5bdc544e20bcf4d2076
Surya Sai Sundeep <suryasaisundeep525@gmail.com>
2024-06-18 10:48:50 +0530

    Update cartRoutes.js

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -7,7 +7,7 @@ const cartApi = require('../apis/cartApis')
 //fetch all records
 router.get("/cart",cartApi.cart_get)
 router.post("/add_to_cart",cartApi.add_to_cart)
-router.post("delete_cart",cartApi.delete_from_cart)
+router.post("/delete_cart",cartApi.delete_from_cart)
 // router.post("/add_to_cart",productApi.add_cart)
 //export router
 module.exports = router
